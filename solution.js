31348093a8953fb901875a31dedae785358eb3c1
Rami <103028944+Rami24t@users.noreply.github.com>
2022-07-06 12:05:44 +0200

    Update solution.js

---
+++ solution.js
@@ -0,0 +1,17 @@
+//1.
+console.log(3==3);
+console.log(3===3);
+console.log(2);
+console.log(7);
+console.log(9);
+//2.
+
+let myVar = true;
+console.log(!myVar?'good morning' : 'good evening' );
+
+let firstName = '' || 'John' || 0;
+let emptyStr  = '' && false && 'Hello World';
+let zero  = '' || false || 0;
+let seven = 75 && 'nine' && 7;
+
+console.log(firstName,zero,emptyStr,seven);
